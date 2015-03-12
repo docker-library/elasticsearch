48be16b576157d74b38202f40ffbab04f02d668f
Christophe Labouisse <christophe@labouisse.org>
2015-03-12 13:31:03 +0100

    Issue #5 make elastisearch run as a normal user.
    - Add a docker-entrypoint.sh script dropping root privileges when
      starting up elasticsearch. The script uses `gosu` to drop the
      permission
    - Add `ENTRYPOINT`, while keeping `CMD` for clarity
    - Fix `update.sh` script to work with the new elastic.co site
    
    Most update is shamelessly borrowed from PostgreSQL official image

--- update.sh
+++ update.sh
@@ -5,9 +5,10 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-downloadable=$(curl -sSL 'http://www.elasticsearch.org/downloads' | sed -rn 's!.*?http://www.elasticsearch.org/downloads/[0-9]+-[0-9]+-[0-9]+/">Download v ([0-9]+\.[0-9]+\.[0-9]+)<.*!\1!gp')
+downloadable=$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | sed -rn 's!.*?/downloads/past-releases/[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\1!gp')
 
 for version in "${versions[@]}"; do
 	recent=$(echo "$downloadable" | grep -m 1 "$version")
-	sed -ri -e 's/^(ENV ELASTICSEARCH_VERSION) .*/\1 '"$recent"'/' "$version/Dockerfile"
+	sed 's/%%VERSION%%/'"$recent"'/' <Dockerfile.template >"$version/Dockerfile"
+	cp -p docker-entrypoint.sh $version
 done
