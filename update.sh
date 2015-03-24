a880d697b2e5bb14992ac69d5d4f3324f160f1a6
Christophe Labouisse <christophe@labouisse.org>
2015-03-24 14:42:47 +0100

    Make update.sh more MacOSX friendly.
    Original behaviour on Linux is unchanged while making it work on
    Mac OS X.

--- update.sh
+++ update.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+if [[  $(uname) == "Darwin" ]]; then
+	SED=gsed
+	RL_OPTS=""
+else
+	SED=sed
+	RL_OPTS="-f"
+fi
+
+cd "$(dirname "$(readlink ${RL_OPTS} "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-downloadable=$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | sed -rn 's!.*?/downloads/past-releases/(elasticsearch-)?[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\2!gp')
+downloadable=$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | ${SED} -rn 's!.*?/downloads/past-releases/[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\1!gp')
 
 for version in "${versions[@]}"; do
 	recent=$(echo "$downloadable" | grep -m 1 "$version")
