04356c816e2c2667b9f08e521a45c4b63c47d51a
Christophe Labouisse <christophe@labouisse.org>
2015-03-26 23:50:16 +0100

    Make entry point more versatile for USER.
    The user and group can now be choosen using the `RUN_AS` environment
    variable. `RUN_AS` should follow the `user[:group]` pattern and both
    user/group numbers and user/group names are accepted.
    
    The `chown`/`gosu` mechanism is skipped when the user is either `0`
    or `root` or if the command is not `elasticsearch`.

--- 1.3/docker-entrypoint.sh
+++ 1.3/docker-entrypoint.sh
@@ -7,11 +7,14 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
 fi
 
-# Drop root privileges if we are running elasticsearch
-if [ "$1" = 'elasticsearch' -a -z "$RUN_AS_ROOT" ]; then
+RUN_AS=${RUN_AS:-elasticsearch:elasticsearch}
+RUN_AS_USER=${RUN_AS/:*/}
+
+# Drop root privileges if we are running elasticsearch and RUN_AS is not root
+if [ "$1" = 'elasticsearch' -a  "$RUN_AS_USER" != '0' -a "$RUN_AS_USER" != 'root' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	exec gosu elasticsearch "$@"
+	chown -R $RUN_AS /usr/share/elasticsearch/data
+	exec gosu $RUN_AS_USER "$@"
 fi
 
 # As argument is not related to elasticsearch,
