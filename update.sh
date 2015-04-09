682f9af694ab0977e1ffc9e061ccf9f44ccc569c
Tristan Carel <tristan@cogniteev.com>
2015-04-09 17:11:59 +0200

    sed executable overridable using SED environment variable.

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
+SED=${SED:-sed}
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
-downloadable=$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | sed -rn 's!.*?/downloads/past-releases/(elasticsearch-)?[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\2!gp')
+downloadable=$(curl -sSL 'https://www.elastic.co/downloads/past-releases' | "$SED" -rn 's!.*?/downloads/past-releases/(elasticsearch-)?[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\2!gp')
 
 for version in "${versions[@]}"; do
 	recent=$(echo "$downloadable" | grep -m 1 "$version")
-	sed 's/%%VERSION%%/'"$recent"'/' <Dockerfile.template >"$version/Dockerfile"
+	"$SED" 's/%%VERSION%%/'"$recent"'/' <Dockerfile.template >"$version/Dockerfile"
 	cp -p docker-entrypoint.sh $version
 done
