1aaead32a1a561fb2b57d8a116e288bb581d9191
Tianon Gravi <admwiggin@gmail.com>
2015-08-12 12:55:42 -0700

    Modify update.sh to use the deb repo indexes directly instead of scraping HTML

--- update.sh
+++ update.sh
@@ -3,26 +3,30 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-versions=( */ )
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
 versions=( "${versions[@]%/}" )
-downloadable=$({
-	# get a few pages worth so we make sure to capture a decent number of releases
-	curl -fsSL 'https://www.elastic.co/downloads/past-releases'
-	curl -fsSL 'https://www.elastic.co/downloads/past-releases?page=2'
-	curl -fsSL 'https://www.elastic.co/downloads/past-releases?page=3'
-} | sed -rn 's!.*?/downloads/past-releases/(elasticsearch-)?[0-9]+-[0-9]+-[0-9]+">Elasticsearch ([0-9]+\.[0-9]+\.[0-9]+)<.*!\2!gp')
 
 travisEnv=
 for version in "${versions[@]}"; do
 	travisEnv='\n  - VERSION='"$version$travisEnv"
 	
-	recent=$(echo "$downloadable" | grep -m 1 "^$version" || true)
-	if [ -z "$recent" ]; then
+	fullVersion="$(curl -fsSL "http://packages.elasticsearch.org/elasticsearch/$version/debian/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" { print $2 }' | sort -rV | head -n1)"
+	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
 		continue
 	fi
-	sed 's/%%VERSION%%/'"$recent"'/' <Dockerfile.template >"$version/Dockerfile"
-	cp -p docker-entrypoint.sh $version
+	(
+		set -x
+		cp docker-entrypoint.sh Dockerfile.template "$version/"
+		mv "$version/Dockerfile.template" "$version/Dockerfile"
+		sed -i '
+			s/%%ELASTICSEARCH_MAJOR%%/'"$version"'/g;
+			s/%%ELASTICSEARCH_VERSION%%/'"$fullVersion"'/g;
+		' "$version/Dockerfile"
+	)
 done
 
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
