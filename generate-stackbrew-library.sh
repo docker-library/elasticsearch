fc12951bb4851abd980395e1004fbfa8729279be
Tianon Gravi <admwiggin@gmail.com>
2015-08-28 12:45:51 -0700

    No '~' in Docker tags

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -17,7 +17,7 @@ echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
 	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-	fullVersion="$(grep -m1 'ENV ELASTICSEARCH_VERSION' "$version/Dockerfile" | cut -d' ' -f3)"
+	fullVersion="$(grep -m1 'ENV ELASTICSEARCH_VERSION' "$version/Dockerfile" | cut -d' ' -f3 | sed 's/~/-/g')"
 	
 	versionAliases=()
 	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
