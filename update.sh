d74baa147f5ed54c2d048bae79a49018c1eae61f
Tianon Gravi <admwiggin@gmail.com>
2015-10-15 10:41:31 -0700

    Fix update.sh and Dockerfile.template to handle the new 2+ URLs

--- update.sh
+++ update.sh
@@ -13,7 +13,12 @@ travisEnv=
 for version in "${versions[@]}"; do
 	travisEnv='\n  - VERSION='"$version$travisEnv"
 	
-	fullVersion="$(curl -fsSL "http://packages.elasticsearch.org/elasticsearch/$version/debian/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" { print $2 }' | sort -rV | head -n1)"
+	repoBase="http://packages.elasticsearch.org/elasticsearch/$version/debian"
+	if [ "${version%%.*}" -ge 2 ]; then
+		repoBase="http://packages.elasticsearch.org/elasticsearch/${version%%.*}.x/debian"
+	fi
+	
+	fullVersion="$(curl -fsSL "$repoBase/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" { print $2 }' | sort -rV | head -n1)"
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
 		continue
@@ -25,6 +30,7 @@ for version in "${versions[@]}"; do
 		sed -i '
 			s/%%ELASTICSEARCH_MAJOR%%/'"$version"'/g;
 			s/%%ELASTICSEARCH_VERSION%%/'"$fullVersion"'/g;
+			s!%%ELASTICSEARCH_REPO_BASE%%!'"$repoBase"'!g;
 		' "$version/Dockerfile"
 	)
 done
