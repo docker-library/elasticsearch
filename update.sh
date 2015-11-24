730ed65afd6f61ffc39c3b4832458e378b49312a
Tianon Gravi <admwiggin@gmail.com>
2015-11-24 10:16:59 -0800

    Whoops, actual 2.0.1

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ for version in "${versions[@]}"; do
 		repoBase="http://packages.elasticsearch.org/elasticsearch/${version%%.*}.x/debian"
 	fi
 	
-	fullVersion="$(curl -fsSL "$repoBase/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" { print $2 }' | sort -rV | head -n1)"
+	fullVersion="$(curl -fsSL "$repoBase/dists/stable/main/binary-amd64/Packages" | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "elasticsearch" && $1 == "Version" && $2 ~ /^'"$version"'/ { print $2 }' | sort -rV | head -n1)"
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
 		continue
