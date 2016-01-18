a3f8e658da60bf58e97e18a78d85815a896f9eed
Tianon Gravi <admwiggin@gmail.com>
2016-01-18 10:59:24 -0800

    Fix minor typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # Drop root privileges if we are running elasticsearch
-# allow the container to be stated with `--user`
+# allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
 	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
