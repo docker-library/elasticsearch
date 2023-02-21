f04f67eeac5a73b49e7c6e9cffadd7554b708f13
Tianon Gravi <admwiggin@gmail.com>
2023-02-21 15:17:05 -0800

    Ignore buildkit-isms during build/history check

--- .github/workflows/history.sh
+++ .github/workflows/history.sh
@@ -10,8 +10,18 @@ docker image history --no-trunc --format '{{ .CreatedBy }}' "$@" \
 		# munge the checksum of the first ADD of the base image (base image changes unnecessarily break our diffs)
 		NR == 1 && $1 == "ADD" && $4 == "/" { $2 = "-" }
 
-		# ignore the contents of certain copies (notoriously unreliable hashes because they often contain timestamps)
-		$1 == "COPY" && ($4 == "/usr/share/elasticsearch" || $4 ~ /^\/opt\/jdk-/) { gsub(/:[0-9a-f]{64}$/, ":filtered-content-hash", $2) }
+		# remove "org.label-schema.build-date" and "org.opencontainers.image.created" (https://github.com/elastic/dockerfiles/pull/101#pullrequestreview-879623350)
+		$1 == "LABEL" { gsub(/ (org[.]label-schema[.]build-date|org[.]opencontainers[.]image[.]created)=[^ ]+( [0-9:+-]+)?/, "") }
+
+		# remove several buildkit-isms
+		/# buildkit$/ {
+			sub(/^RUN[[:space:]]+/, "")
+			sub(/^\/bin\/sh[[:space:]]+-c[[:space:]]+/, "")
+			sub(/[[:space:]]*# buildkit$/, "")
+		}
+		$1 == "EXPOSE" { gsub(/map\[|\/tcp:\{\}|\]/, "") }
+		# buildkit makes COPY expressions hyper-simplified ("COPY --chown=1000:0 --from=foo /foo /bar" becomes just "COPY /foo /bar" vs "COPY --chown=1000:0 dir:xxxx in /bar")
+		$1 == "COPY" { $0 = gensub(/^[[:space:]]*(COPY)[[:space:]]+([^[:space:]]+[[:space:]]+)+([^[:space:]]+)[[:space:]]*$/, "\\1 ... \\3", 1) }
 
 		# sane and sanitized, print it!
 		{ print }
