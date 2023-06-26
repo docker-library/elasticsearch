b57fb36f0db4f7969999ddf3781d55ec2e7210e5
Tianon Gravi <admwiggin@gmail.com>
2023-06-26 13:39:23 -0700

    Fix "Upstream Diff"

--- .github/workflows/history.sh
+++ .github/workflows/history.sh
@@ -8,7 +8,7 @@ docker image history --no-trunc --format '{{ .CreatedBy }}' "$@" \
 	| sed -r 's!^/bin/sh[[:space:]]+-c[[:space:]]+(#[(]nop[)][[:space:]]+)?!!' \
 	| gawk '
 		# munge the checksum of the first ADD of the base image (base image changes unnecessarily break our diffs)
-		NR == 1 && $1 == "ADD" && $4 == "/" { $2 = "-" }
+		$1 == "ADD" && $4 == "/" { $2 = "-" }
 
 		# remove "org.label-schema.build-date" and "org.opencontainers.image.created" (https://github.com/elastic/dockerfiles/pull/101#pullrequestreview-879623350)
 		$1 == "LABEL" { gsub(/ (org[.]label-schema[.]build-date|org[.]opencontainers[.]image[.]created)=[^ ]+( [0-9:+-]+)?/, "") }
